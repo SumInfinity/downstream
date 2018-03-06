d115c88464a6dee8ad838f052eb6d3705cfaf5f1
SumInfinity <ekenemadunagu@gmail.com>
2018-03-06 20:37:04 +0100

    captured route

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { PricesService } from '../prices.service'
+import { PricesService } from '../prices.service';
+import { AngularFireList } from "angularfire2/database";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-company',
@@ -7,10 +9,15 @@ import { PricesService } from '../prices.service'
   styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
+  public prices : AngularFireList<any> ;
+  public companyId: string;
 
-  constructor(private pricesService: PricesService) { }
+  constructor(private pricesService: PricesService, private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.companyId = this.activeRoute.snapshot.params['id'];
+    this.prices = this.pricesService.getSingleCompany(this.companyId);
+    //TODO: deciper company model and insert it here
   }
 
 }
